98a343ab8f8d94b810fbc4d7e8094781c1573cde
PhongxTran <92942665+PhongxTran@users.noreply.github.com>
2022-04-22 10:04:42 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import serial.tools.list_ports
+import json
+import time
+import paho.mqtt.client as mqttclient
+print("IoT Gateway")
+
+BROKER_ADDRESS = "demo.thingsboard.io"
+PORT = 1883
+mess = ""
+
+# TODO: Add your token and your comport
+# Please check the comport in the device manager
+THINGS_BOARD_ACCESS_TOKEN = "3RIQ53PUutt8exKvoljb"
+bbc_port = "COM4"
+if len(bbc_port) > 0:
+    ser = serial.Serial(port=bbc_port, baudrate=115200)
+
+
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    # TODO: Add your source code to publish data to the server
+    if splitData[1] == "TEMP":
+        temp = splitData[2]
+        tempData = {"temperature": temp}
+        client.publish('v1/devices/me/telemetry', json.dumps(tempData), 1)
+    if splitData[1] == "LIGHT":
+        light = splitData[2]
+        lightData = {"light": light}
+        client.publish('v1/devices/me/telemetry', json.dumps(lightData), 1)
+
+
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+
+
+def subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed...")
+
+
+def recv_message(client, userdata, message):
+    print("Received: ", message.payload.decode("utf-8"))
+    cmd = 1
+    # TODO: Update the cmd to control 2 devices
+    try:
+        jsonobj = json.loads(message.payload)
+        if jsonobj['method'] == "setLED":
+            # temp_data = {'value': True}
+            # temp_data['value'] = jsonobj['params']
+            temp_data = {'valueLED': True}
+            temp_data['valueLED'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+            if (jsonobj['params']):
+                cmd = "2"
+            else:
+                cmd = "3"
+        if jsonobj['method'] == "setFAN":
+            # temp_data = {'value': True}
+            # temp_data['value'] = jsonobj['params']
+            temp_data = {'valueFAN': True}
+            temp_data['valueFAN'] = jsonobj['params']
+            client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+            if (jsonobj['params']):
+                cmd = "4"
+            else:
+                cmd = "5"
+    except:
+        pass
+    if len(bbc_port) > 0:
+        ser.write((str(cmd) + "#").encode())
+
+
+def connected(client, usedata, flags, rc):
+    if rc == 0:
+        print("Thingsboard connected successfully!!")
+        client.subscribe("v1/devices/me/rpc/request/+")
+    else:
+        print("Connection is failed")
+
+
+client = mqttclient.Client("Gateway_Thingsboard")
+client.username_pw_set(THINGS_BOARD_ACCESS_TOKEN)
+
+client.on_connect = connected
+client.connect(BROKER_ADDRESS, 1883)
+client.loop_start()
+
+client.on_subscribe = subscribed
+client.on_message = recv_message
+
+
+while True:
+
+    if len(bbc_port) > 0:
+        readSerial()
+
+    time.sleep(1)
